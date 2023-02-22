f7f49a0c304f5b9016f8f7be9873ce194e48f0e7
Hemantha Dhanushka <hemantha@tutopiya.com>
2023-02-23 03:34:02 +0530

    Create add API action

--- pages/api/employee.js
+++ pages/api/employee.js
@@ -1,8 +1,9 @@
 import dbConnect from '@/config/database';
-import Employee from '../../models/Employee';
+import regex from '@/config/regex';
+import Employee from '@/models/Employee';
 
 const handler = async (req, res) => {
-  const { method, query } = req;
+  const { method, query, body } = req;
 
   await dbConnect();
 
@@ -12,16 +13,50 @@ const handler = async (req, res) => {
         const employees = await Employee.find({});
         res.status(200).json(employees);
       } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
+        res.status(400).json({ message: error.message });
       }
       break;
 
-    case 'GET':
+    case 'POST':
+      const { firstName, lastName, email, number, gender } = body;
+      const { nameRegex, emailRegex, genderRegex, phoneRegex } = regex;
+
       try {
-        const employees = await Employee.find({});
-        res.status(200).json(employees);
+        if (!firstName || !lastName || !email || !number || !gender) {
+          return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        if (!firstName.match(nameRegex)) {
+          return res.status(400).json({ message: 'First name is invalid' });
+        }
+
+        if (!lastName.match(nameRegex)) {
+          return res.status(400).json({ message: 'Last name is invalid' });
+        }
+
+        if (!email.match(emailRegex)) {
+          return res.status(400).json({ message: 'Email is invalid' });
+        }
+
+        if (!number.match(phoneRegex)) {
+          return res.status(400).json({ message: 'Phone number is invalid' });
+        }
+
+        if (!gender.match(genderRegex)) {
+          return res.status(400).json({ message: 'Gender is invalid' });
+        }
+
+        const employee = await Employee.create({
+          firstName,
+          lastName,
+          email,
+          gender,
+          number,
+        });
+
+        res.status(200).json(employee);
       } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
+        res.status(400).json({ message: error.message });
       }
       break;
 
